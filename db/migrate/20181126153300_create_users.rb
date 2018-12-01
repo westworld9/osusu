6024452755abb44a88d00c5837a972cd48c25460
shamoto <thebicameralmind9@gmail.com>
2018-12-01 04:47:56 +0000

    データベース修正

--- db/migrate/20181126153300_create_users.rb
+++ db/migrate/20181126153300_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name 
       t.string :email 
-      t.string :password_digest
+      t.string :passwprd_digest
       t.timestamps
     end
   end
